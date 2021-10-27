f370e4f8eff2d9d17729f7ace98de818a58fe3ca
Timothy Graupmann <tgraupmann@gmail.com>
2021-10-27 10:44:56 -0700

    Create sample project

---
+++ public/vue_components.js
@@ -0,0 +1,74 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// item: panel
+Vue.component(
+    'thing-item', {
+    props: ['name', 'type'],
+    methods: {
+    },
+    template: `
+    <div class="item-thing">
+      <span class="item-thing-name">Name: {{name}}</span>
+      <span class="item-thing-type">Type: {{type}}</span>
+    </span>
+    `,
+    data() {
+        return this.$root;
+    }
+});
+// list: thing
+Vue.component(
+    'thing-list', {
+    props: ['children'],
+    methods: {
+    },
+    template: `
+    <div>
+      <thing-item v-for="item in children"
+        :name="item.name"
+        :type="item.type">
+      </thing-item>
+    </div>
+    `,
+    data() {
+        return this.$root;
+    }
+});
+
+// section: panel
+Vue.component(
+    'panel-section', {
+    props: [],
+    methods: {
+    },
+    template: `
+    <div>
+      <thing-list :children="$root._data.things">
+      </thing-list>
+    </div>
+    `,
+    data() {
+        return this.$root;
+    }
+});
+
+var vue = undefined;
+$(document).ready(function () {
+    vue = new Vue({
+        el: '#root',
+        data: {
+            things: [
+                {
+                    "name": "hello",
+                    "type": "world"
+                },
+                {
+                    "name": "thing1",
+                    "type": "thing2"
+                }
+            ]
+        }
+    });
+});
